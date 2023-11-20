11f735562919891d6898d9c69991b7cfd12b7f81
AlkarizqyRPB <alkarizqyrestupb@gmail.com>
2023-11-20 23:03:52 +0700

    diperbaharui

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,53 @@
 // service-worker.js
 const CACHE_NAME = 'rental-mobil-cache';
 const urlsToCache = [
-  '/',
+  '/RentalMobilAlkarizqy.github.io/',
   'index.html',
   'style.css',
   'script.js',
   // tambahkan file-file lain yang perlu di-cache
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  console.log("masuk");
+  deferredPrompt = e;
+  showInstallPromotion();
+});
+
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request);
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log("[ServiceWorker] Hapus cache lama", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
